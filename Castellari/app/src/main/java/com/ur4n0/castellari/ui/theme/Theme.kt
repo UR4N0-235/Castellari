ab52ab41721b9dd25ddd1429d7e6290d8dba3e38
Matheus Fernandes <matheus.fernandes92.235@protonmail.com>
2023-01-28 16:55:01 -0300

    refactor(removed darktheme): removed autochange to darktheme
    Android studio generate in theme file, an function that chage to darktheme automatically, but this
    is unused in this project, so, this commit remove it

--- Castellari/app/src/main/java/com/ur4n0/castellari/ui/theme/Theme.kt
+++ Castellari/app/src/main/java/com/ur4n0/castellari/ui/theme/Theme.kt
@@ -1,41 +1,17 @@
 package com.ur4n0.castellari.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
     primary = YellowCastellari,
     primaryVariant = BlueCastellari,
     secondary = Teal200,
-
-)
-
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
 )
 
 @Composable
-fun CastellariTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-//    val colors = if (darkTheme) {
-//        DarkColorPalette
-//    } else {
-//        LightColorPalette
-//    }
-
+fun CastellariTheme(content: @Composable () -> Unit) {
     MaterialTheme(
         colors = DarkColorPalette,
         typography = Typography,
